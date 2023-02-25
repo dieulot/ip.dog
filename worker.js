ffbdc6e5d35cd1cdfdad58664581057ae786272a
Alexandre Dieulot <adieulot@gmail.com>
2023-02-25 11:48:40 +0100

    imported worker: dynamically load translations

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
 import indexTemplate from './index.html'
 
+import translationDe from './translations/de.txt'
+import translationEn from './translations/en.txt'
+import translationFr from './translations/fr.txt'
+import translationHi from './translations/hi.txt'
+
 export default {
   fetch: handleRequest,
 }
@@ -73,46 +78,10 @@ const indexHtml = indexTemplate
 
 const translations = {}
 
-translations['de'] = `Zum kopieren irgendwo hinklicken
-Kopiert!
-ip.dog is der schnellste Weg um deine IP Adresse sicher zu bekommen. 🐶
-Super-kurze domain.
-Die gesamte Seite ist der "kopieren" Button, deine Maus/dein Finger muss sich also nicht weit bewegen! Du kannst auch eine Taste drücken, um deine IP Adresse zu kopieren!
-Dies ist eine dynamische Seite mit der Geschwindigkeit einer statischen Seite, verteilt über den ganzen Globus, möglich gemacht durch Cloudflare Workers.
-Die gesamte Seite passt in ein einziges TCP Paket (≤ 1460 Bytes) um Netzrundfahrten zu vermeiden.
-Mehrsprachig (das <a href="/translate">Übersetzen</a> dauert nur 2 Minuten), dark mode, barrierefrei.
-Entwickelt von <a href="https://dieulot.fr/">Alexandre Dieulot</a>. <a href="https://github.com/dieulot/ip.dog">Quellcode</a> verfügbar.
-`
-translations['en'] = `Click anywhere to copy
-Copied
-ip.dog is the fastest way to get your IP address securely. 🐶
-Extra-short domain name.
-The whole page is the copy button, or you can just press a key, so your mouse/finger doesn’t have to travel.
-It’s a dynamic site with the speed of a static one, distributed near you anywhere in the world, thanks to Cloudflare Workers.
-The whole site fits in a single TCP packet (≤ 1460 bytes) to avoid network round trips.
-Multilingual (<a href="/translate">translating</a> takes two minutes), dark mode, accessible.
-Made by <a href="https://dieulot.fr/">Alexandre Dieulot</a>. <a href="https://github.com/dieulot/ip.dog">Source code</a> available.
-`
-translations['fr'] = `Cliquez n’importe où pour copier
-Copié
-ip.dog est le moyen le plus rapide d’obtenir votre adresse IP de façon sécurisée. 🐶
-Nom de domaine super court.
-La page entière est le bouton pour copier, ou vous pouvez simplement appuyer sur une touche, ainsi votre souris/doigt n’a pas à se déplacer.
-C’est un site dynamique avec la vitesse d’un site statique, distribué près de chez vous partout dans le monde, grâce à Cloudflare Workers.
-Le site entier tient dans un seul paquet TCP (≤ 1460 octets) pour éviter les aller-retours réseau.
-Multilingue (<a href="/translate">traduire</a> prend deux minutes), thème sombre, accessible.
-Conçu par <a href="https://dieulot.fr/">Alexandre Dieulot</a>. <a href="https://github.com/dieulot/ip.dog">Code source</a> disponible.
-`
-translations['hi'] = `कहीं भी कॉपी करने के लिए क्लिक करें
-कॉपी किया गया
-ip.dog आपके आईपी पते को सुरक्षित रूप से प्राप्त करने का सबसे तेज़ तरीका है। 🐶
-अतिरिक्त-संक्षिप्त डोमेन नाम।
-पूरा पृष्ठ कॉपी बटन है, या आप केवल एक कुंजी दबा सकते हैं, इसलिए आपके माउस / उंगली को यात्रा नहीं करनी होगी।
-यह एक गतिशील साइट है जिसकी गति स्थिर है, जिसे क्लाउडफ़ेयर वर्कर्स की बदौलत दुनिया में कहीं भी वितरित किया जा सकता है।
-नेटवर्क राउंड ट्रिप से बचने के लिए पूरी साइट एक एकल टीसीपी पैकेट (by 1460 बाइट्स) में फिट होती है।
-बहुभाषी (<a href="/translate"> अनुवाद करना </a> दो मिनट लगते हैं), डार्क मोड, सुलभ।
-<a href="https://dieulot.fr/"> अलेक्जेंड्रे डाईलुओट </a> द्वारा बनाया गया। <a href="https://github.com/dieulot/ip.dog"> स्रोत कोड </a> उपलब्ध है।
-`
+translations['de'] = translationDe
+translations['en'] = translationEn
+translations['fr'] = translationFr
+translations['hi'] = translationHi
 
 
 const translateHtml = `<!doctype html>
